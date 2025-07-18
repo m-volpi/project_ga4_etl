f9e5854e6a76b69948881452477265208f27091c
m-volpi <lma.volpi@gmail.com>
2025-07-18 17:09:45 -0300

    Remover executar_ga4.bat do controle do Git

--- executar_ga4.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "C:\Users\TI\OneDrive - Cairo Benevides Industria de Confeccoes do vestuario Ltda\APIs\GA4_Google_Analytics"
-call "C:\ProgramData\anaconda3\Scripts\activate.bat"
-python main.py
